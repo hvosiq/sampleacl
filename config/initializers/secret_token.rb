847078fb62d452d484b82d11504f0197afb56e37
DragonsIQ <dragons@solutionsiq.com>
2013-12-04 16:10:44 -0800

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleacl::Application.config.secret_token = '828db7fd7af43f171a471111feabfaec16db92af035c056eb8093af19d1b7bf2e875b855b75d6b11089c44edbd0a91af5e5dcaeeab4d836942be531821270d71'
